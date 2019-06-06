d46705ebe8abfaef1ff4461a0fb6eb94f3cbb299
猫小毛 <moskylit@gmail.com>
2019-06-06 18:11:32 +0800

    复习：day 1

--- 《C 语言程序设计》课后习题/1.6 三个值输出最大.c
+++ 《C 语言程序设计》课后习题/1.6 三个值输出最大.c
@@ -1,18 +1,19 @@
+/* 默认将第一个数看成最大的，然后与之比较 */
 #include<stdio.h>
 int main()
 {
     printf("Please enter three numbers(use gap to split number):\n");
-    int a,b,c,max;
-    scanf("%d %d %d",&a,&b,&c);
-    max=a;
-    if(max<b)
+    int a, b, c, max;
+    scanf("%d %d %d", &a, &b, &c);
+    max = a;
+    if(max < b)
     {
-        max=b;
+        max = b;
     }
-    if(max<c)
+    if(max < c)
     {
-        max=c;
+        max = c;
     }
-    printf("MAX=%d",max);
+    printf("MAX = %d", max);
     return 0;
 }
--- 《C 语言程序设计》课后习题/2.4.2 十个数求最大数.c
+++ 《C 语言程序设计》课后习题/2.4.2 十个数求最大数.c
@@ -1,22 +1,23 @@
+/* 用数组存放十个数，默认第一个最大，然后与之比较 */
 #include<stdio.h>
 int main()
 {
     printf("Enter 10 numbers:\n");
     int num[10];
-    int i,max;
-    for(i=0;i<=9;i++)
+    int i, max;
+    for(i = 0; i <= 9; i++)
     {
-        scanf("%d",&num[i]);
-        printf("inputed %d num:%d\n",i+1,num[i]);
+        scanf("%d", &num[i]);
+        printf("inputed %d num:%d\n", i+1, num[i]);
     }
-    max=num[0];
-    for(i=1;i<=9;i++)
+    max = num[0];
+    for(i = 1; i <= 9; i++)
     {
-        if(max<num[i])
+        if(max < num[i])
         {
-            max=num[i];
+            max = num[i];
         }
     }
-    printf("MAX=%d",max);
+    printf("MAX = %d", max);
     return 0;
 }
