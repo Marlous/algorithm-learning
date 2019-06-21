51da76d662776bd2bd29672c603aa3966e84d152
猫小毛 <moskylit@gmail.com>
2019-06-21 17:11:37 +0800

    复习：day 8

--- 《C 语言程序设计》课后习题/5.17 两队组合比赛名单.c
+++ 《C 语言程序设计》课后习题/5.17 两队组合比赛名单.c
@@ -1,21 +1,22 @@
-/* 甲队ABC三人，乙队XYZ三人，组队乒乓球比赛。但A不和X，C不和XY */
+/* 甲队ABC三人，乙队XYZ三人，组队乒乓球比赛。但A不和X，C不和XY
+用数组装数据，两层循环组合出所有可能，只输出符合的情况*/
 #include<stdio.h>
 int main()
 {
-    int i,j;
-    char a[3]={'A','B','C'};
-    char b[3]={'X','Y','Z'};
-    for(i=0;i<=2;i++)
+    int i, j;
+    char a[3] = {'A', 'B', 'C'};
+    char b[3] = {'X', 'Y', 'Z'};
+    for(i = 0; i <= 2; i++)
     {
-        for(j=0;j<=2;j++)
+        for(j = 0; j <= 2; j++)
         {
-            if((a[i]=='A' && b[j]=='X') || (a[i]=='C' && b[j]=='X') || (a[i]=='C' && b[j]=='Z'))
+            if((a[i] == 'A' && b[j] == 'X') || (a[i] == 'C' && b[j] == 'X') || (a[i] == 'C' && b[j] == 'Y'))
             {
                 //
             }
             else
             {
-                printf("%c%c ",a[i],b[j]);
+                printf("%c%c ", a[i], b[j]);
             }
         }
     }
