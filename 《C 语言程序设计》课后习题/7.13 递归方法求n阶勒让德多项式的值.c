deae781324c4becf710a9a1c2987cbeab79e7029
猫小毛 <moskylit@gmail.com>
2019-07-03 16:39:43 +0800

    复习：day 15

--- 《C 语言程序设计》课后习题/7.13 递归方法求n阶勒让德多项式的值.c
+++ 《C 语言程序设计》课后习题/7.13 递归方法求n阶勒让德多项式的值.c
@@ -1,29 +1,32 @@
+/* 书中给出了公式，直接带入即可 */
 #include<stdio.h>
-float p(int n,float x);
+float p(int n, float x);
 int main()
 {
     printf("Enter n,x:\n");
     int n;
     float x;
-    scanf("%d %f",&n,&x);
-    printf("value:%f",p(n,x));
+
+    scanf("%d %f", &n, &x);
+    printf("value:%f", p(n, x));
+
     return 0;
 }
 
-float p(int n,float x)
+float p(int n, float x)
 {
     float num;
-    if(n==0)
+    if(n == 0)
     {
-        num=1;
+        num = 1;
     }
-    else if(n==1)
+    else if(n == 1)
     {
-        num=x;
+        num = x;
     }
-    else if(n>1)
+    else if(n > 1)
     {
-        num=((2*n-1)*x-p(n-1,x)-(n-1)*p(n-2,x))/n;
+        num = ((2 * n - 1) * x - p(n - 1, x) - (n - 1) * p(n - 2, x)) / n;
     }
     return num;
 }
--- 《C 语言程序设计》课后习题/7.16 十六进制转十进制.c
+++ 《C 语言程序设计》课后习题/7.16 十六进制转十进制.c
@@ -1,3 +1,7 @@
+/* 以十六进制数字用字符串读入数组，
+判断出长度，
+将数组倒序，数组下标正好等于要乘的次方数 a[i] X 16 的 i 次方，
+注意将字符映射计算成对应的数字，然后累加 */
 #include<stdio.h>
 #include<math.h>
 #include<string.h>
@@ -7,43 +11,46 @@ int main()
 {
     printf("Enter Hex number:\n");
     char a[N];
-    scanf("%s",&a);
-    printf("\n%d\n",HToD(a));
+
+    scanf("%s", &a);
+    printf("\n%d\n", HToD(a));
+
     return 0;
 }
 
 int HToD(char a[])
 {
-    int num=0;
+    int num = 0;
     char temp;
-    int count,i;
-    count=strlen(a);
+    int count, i;
+    count = strlen(a);
 
 
-    char *head=&a[0],*rear=&a[count-1];  //将数组元素倒置
-    for(i=0;i<count;i++)
+    char *head = &a[0], *rear = &a[count - 1];  //将数组元素倒置
+    for(i = 0; i < count; i++)
     {
-        temp=*head;
-        *head=*rear;
-        *rear=temp;
-        head++;
+        temp = *head;
+        *head = *rear;
+        *rear = temp;
+        head ++;
     }
 
 
-    for(i=0;i<count;i++) //依次取每一个出来
+    for(i = 0; i < count; i++) //依次取每一个出来
     {
-        if(a[i]>='0' && a[i]<='9')
+        if(a[i] >= '0' && a[i] <= '9')
         {
-            num=num+(a[i]-48)*pow(16,i);
+            num = num + (a[i] - 48) * pow(16, i);
         }
-        else if(a[i]>='A' && a[i]<='F')
+        else if(a[i] >= 'A' && a[i] <= 'F')
         {
-            num=num+(a[i]-55)*pow(16,i);
+            num = num + (a[i] - 55) * pow(16, i);
         }
-        else if(a[i]>='a' && a[i]<='f')
+        else if(a[i] >= 'a' && a[i] <= 'f')
         {
-            num=num+(a[i]-87)*pow(16,i);
+            num = num + (a[i] - 87) * pow(16, i);
         }
     }
+
     return num;
 }
